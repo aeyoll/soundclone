8f1509fb3adfe6ffb65db1c61434d7a3b484d0a3
Jean-Philippe Bidegain <jp@bidega.in>
2023-06-22 12:51:26 +0200

    docs: fix django version

--- soundclone/urls.py
+++ soundclone/urls.py
@@ -2,7 +2,7 @@
 soundclone URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 
 Examples
 --------
