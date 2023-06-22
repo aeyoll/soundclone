077c0ca1b97ada81e20ed76e8ad6f5b6531e0bb9
Jean-Philippe Bidegain <jp@bidega.in>
2023-06-22 12:49:26 +0200

    refactor: set api path

--- soundclone/urls.py
+++ soundclone/urls.py
@@ -29,7 +29,7 @@ router.register(r'songs', SongViewSet)
 router.register(r'versions', VersionViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
