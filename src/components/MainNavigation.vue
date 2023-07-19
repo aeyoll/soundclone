87b8508a399c4cc228d8c9fbc891c9a9ba8a997d
Jean-Philippe Bidegain <jp@bidega.in>
2023-07-19 09:02:05 +0200

    feat: use a separate component for navigation

---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/upload">Upload</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
