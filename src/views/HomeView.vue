735939db49475b53d1edabc6387b9c84bf580b2c
Jean-Philippe Bidegain <jp@bidega.in>
2023-07-19 09:02:33 +0200

    feat: fetch songs in the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,59 @@
 <script setup lang="ts">
 import { inject, onMounted, Ref, ref } from 'vue';
-import type { AlbumSerializer } from '../../types/core';
+import type { PlaylistSerializer, SongSerializer } from '../../types/core';
 
 const axios: any = inject('axios');
-const albums: Ref<AlbumSerializer[]> = ref([]);
+const playlists: Ref<PlaylistSerializer[]> = ref([]);
+
+const songs: Ref<SongSerializer[]> = ref([]);
+
+const getPlaylists = async () => {
+  const { data } = await axios.get('playlists/');
+  playlists.value = data;
+};
+
+const getSongs = async () => {
+  const { data } = await axios.get('songs/');
+  songs.value = data;
+}
 
 onMounted(async () => {
-  const { data } = await axios.get('albums/');
-  albums.value = data;
+  await Promise.all([getSongs(), getPlaylists()]);
 });
 </script>
 
 <template>
   <main>
-    <h1>This is the home page</h1>
+    <h1>Playlists</h1>
 
-    <div v-for="album in albums" :key="album.id">
-      {{ album.name }}
+    <div v-if="playlists.length > 0" >
+      <div v-for="playlist in playlists" :key="playlist.id">
+        {{ playlist.name }}
 
-      <div v-for="song in album.songs" :key="song.id">
-        {{ song.name }}
+        <div v-for="song in playlist.songs" :key="song.id">
+          {{ song.name }}
+        </div>
       </div>
+    </div>
+
+    <div v-else class="alert alert-danger">
+      No playlist
+    </div>
+
+    <h1>Songs</h1>
+
+    <div v-if="songs.length > 0" >
+      <div v-for="song in songs" :key="song.id">
+        {{ song.name }}
+
+        <div v-for="song in songs" :key="song.id">
+          {{ song.name }}
+        </div>
+      </div>
+    </div>
+
+    <div v-else class="alert alert-danger">
+      No songs
     </div>
   </main>
 </template>
